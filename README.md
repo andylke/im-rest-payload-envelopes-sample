d7a92644f3da099e2cf5e8255148541966c9c3b6
Andy Lian <andylian@silverlakeaxis.com>
2022-03-31 09:37:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# I'm REST Payload Envelopes Sample
+
+This is a sample project showing how to use `I'm REST Payload Envelopes`.
+
+---
+
+To run the project:
+
+```
+mvn spring-boot:run
+```
---
+++ src/test/java/im/rest/payload/envelopes/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package im.rest.payload.envelopes.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
